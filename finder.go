5f73e229191b8d049fdbd8a0f0052abe8c360aca
kaabima <bimantara.hanumpraja@gmail.com>
2020-11-27 20:46:10 +0700

    add view and subs count feature

--- finder.go
+++ finder.go
@@ -5,19 +5,23 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"google.golang.org/api/googleapi/transport"
 	"google.golang.org/api/youtube/v3"
 )
 
+// Variables declaration
 var (
 	query      = flag.String("query", "Google", "Search term")
 	maxResults = flag.Int64("max-results", 25, "Max Youtube results")
 )
 
-const developerKey = "DEVELOPER KEY"
+// Constant declaration
+const developerKey = "DEVELOPER_KEY"
 const youtubeHome = "https://www.youtube.com/watch?v="
 
+// Function declaration
 func handleError(err error, message string) {
 	if message == "" {
 		message = "Error making API call"
@@ -27,17 +31,58 @@ func handleError(err error, message string) {
 	}
 }
 
-// Print the ID and title of each result in a list as well as a name that
-// identifies the list. For example, print the word section name "Videos"
-// above a list of video search results, followed by the video ID and title
-// of each matching video.
+func videoCount(id string, key []string, s *youtube.Service) uint64 {
+	var count uint64
+	call := s.Videos.List(key).
+		Id(id).
+		MaxResults(*maxResults)
+	response, err := call.Do()
+	handleError(err, "")
+
+	for _, item := range response.Items {
+		switch item.Kind {
+		case "youtube#video":
+			count = item.Statistics.ViewCount
+		}
+	}
+	return count
+}
+
+func subscriberCount(id string, key []string, s *youtube.Service) uint64 {
+	var count uint64
+	call := s.Channels.List(key).
+		Id(id).
+		MaxResults(*maxResults)
+
+	response, err := call.Do()
+	handleError(err, "")
+
+	for _, item := range response.Items {
+		switch item.Kind {
+		case "youtube#channel":
+			count = item.Statistics.SubscriberCount
+		}
+	}
+	return count
+}
+
 func printVideoInformation(sectionName string, matches map[string]string) {
 	fmt.Printf("%v:\n", sectionName)
-	for id, title := range matches {
-		fmt.Printf("%v\nURL: %v\n\n", title, youtubeHome+id)
+	for key, value := range matches {
+		switch true {
+		case strings.Contains(key, "view_count"):
+			fmt.Printf("View count: %v\n", value)
+		case strings.Contains(key, "subs_count"):
+			fmt.Printf("Subscriber count: %v\n", value)
+		default:
+			fmt.Printf("\n\n%v\nURL: %v\n", value, youtubeHome+key)
+		}
 	}
 	fmt.Printf("\n\n")
 }
+
+// Another calculation function
+// still progress
 
 func main() {
 	flag.Parse()
@@ -52,24 +97,32 @@ func main() {
 	}
 
 	searchKey := []string{"id", "snippet"}
+	statsKey := []string{"statistics"}
 
 	// Make the API call to Youtube.
-	call := service.Search.List(searchKey).
+	searchCall := service.Search.List(searchKey).
 		Q(*query).
 		MaxResults(*maxResults)
 
-	response, err := call.Do()
+	searchResponse, err := searchCall.Do()
 	handleError(err, "")
 
-	// Group video, channel, and playlist results in separate lists.
+	// Group video in separate lists
 	videos := make(map[string]string)
 
 	// Iterate through each item and add it to the correct list.
-	for _, item := range response.Items {
+	for _, item := range searchResponse.Items {
 		switch item.Id.Kind {
 		case "youtube#video":
 			videos[item.Id.VideoId] = item.Snippet.Title
+			videos["view_count_"+item.Id.VideoId] = fmt.Sprint(
+				videoCount(item.Id.VideoId, statsKey, service),
+			)
+			videos["subs_count_"+item.Id.VideoId] = fmt.Sprint(
+				subscriberCount(item.Id.VideoId, statsKey, service),
+			)
 		}
 	}
 	printVideoInformation("Videos", videos)
+
 }
